42868917eb17a8e5cd87c230b4f6a7a721cb0ab9
sultanbekuly <chingiz.sultanbekuly@gmail.com>
2023-01-10 22:07:14 +0500

    CtCI - Ch1 - 3 - added in place solution

--- Cracking_the_Coding_Interview/Chapter 01/3_URLify.py
+++ Cracking_the_Coding_Interview/Chapter 01/3_URLify.py
@@ -3,18 +3,36 @@
 #length of the string. (Note: If implementing in Java, please use a character array so that you can
 #perform this operation in place.)
 #EXAMPLE
-#Input: "Mr John Smith ", 13
+#Input:  "Mr John Smith    ", 13
 #Output: "Mr%20John%20Smith"
 
-def URLify(str, len):
-    print(str, "-", len)
+#TC is O(N)
+#Uses new string so SC is O(N)
+def URLify(str, length):
+    print(str, "-", length)
     new_str = ""
-    for i in range(len):
+    for i in range(length):
         if(str[i]==" "):
             new_str += "%20"
         else:
             new_str += str[i]
     return new_str
+print(URLify("Mr John Smith    ", 13))
 
-print(URLify("Mr John Smith ", 13))
+#TC is O(N)
+#SC is O(1)
+def URLify2(str, length): #str is list of chars
+    print(str, "-", length)
+    j = len(str)-1
+    for i in reversed(range(length)):
+        if(str[i]==" "):
+            str[j-0] = "0"
+            str[j-1] = "2"
+            str[j-2] = "%"
+            j -= 3
+        else:
+            str[j] = str[i]
+            j -= 1
+    return str
+print(URLify2(list("Mr John Smith    "), 13))
 
