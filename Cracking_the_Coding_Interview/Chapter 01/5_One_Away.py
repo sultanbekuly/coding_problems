39432a730ed41ed067f328e7d7bcbf200f6f9a11
sultanbekuly <chingiz.sultanbekuly@gmail.com>
2023-01-13 20:48:59 +0500

    CtCi - Ch1 - 5 - solved

--- Cracking_the_Coding_Interview/Chapter 01/5_One_Away.py
+++ Cracking_the_Coding_Interview/Chapter 01/5_One_Away.py
@@ -13,12 +13,16 @@
 # Iterate through loop and increment the pointers
 # use them to get string index and check if they are the same
 # if yes ok
-# if not check i+1 == j or i == j+1
+# if not check i+1 == j or i == j+1 or i+1==j+1
 #  if so update index accordingly
 # else return false
 
 # TC - O(N)
 # SC - O(1)
+
+#IndexError: string index out of range
+# =>i_allowed = (i+1 <= len(str1)-1)
+#   j_allowed = (j+1 <= len(str2)-1)
 
 def One_Away(str1, str2):
     print(str1," ; ",str2)
@@ -26,30 +30,32 @@ def One_Away(str1, str2):
         return False
     i = 0
     j = 0
-    smallest_len = 0
-    if(len(str1) < len(str2)):
-        smallest_len = len(str1)
-    else:
-        smallest_len = len(str2)
+    #smallest_len = min(len(str1), len(str2))#used to iterate through loop
     num_of_edits = 0
-    for _ in range(smallest_len):
+    while(True):
         if(str1[i] == str2[j]):
             i += 1
             j += 1
-        elif(str1[i+1] == str2[j] and num_of_edits==0):
-            i+=2
-            j+=1
-            num_of_edits += 1
-        elif(str1[i] == str2[j+1] and num_of_edits==0):
-            i+=1
-            j+=2
-            num_of_edits += 1
-        elif(str1[i+1] == str2[j+1] and num_of_edits==0):
-            i+=2
-            j+=2
-            num_of_edits += 1
+        elif(num_of_edits==0):
+            i_allowed = (i+1 <= len(str1)-1)
+            j_allowed = (j+1 <= len(str2)-1)
+            
+            if(str1[i+1] == str2[j] and i_allowed):
+                i+=2
+                j+=1
+                num_of_edits += 1
+            elif(str1[i] == str2[j+1] and j_allowed):
+                i+=1
+                j+=2
+                num_of_edits += 1
+            elif(str1[i+1] == str2[j+1] and i_allowed and j_allowed):
+                i+=2
+                j+=2
+                num_of_edits += 1
         else:
             return False
+        if(not(i <= len(str1)-1) or not(j <= len(str2)-1)):
+            break
     return True
             
 
@@ -59,5 +65,12 @@ print(One_Away("pale", "ple")) #true
 print(One_Away("pales", "pale")) #true
 print(One_Away("pale", "bale")) #true
 print(One_Away("pale", "bake")) #false
+print(One_Away("pakz", "bake")) #false # checking the last char check
 
 
+
+#abs(-1) => 1
+#min(0, 2) => 0
+# result = zip(['Java', 'Python', 'JavaScript'], [14, 3, 6])
+# print(list(result))
+# print(result)
