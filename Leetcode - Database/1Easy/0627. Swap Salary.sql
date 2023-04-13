387393b480d0dceff77653cd1cfdd4477492d8b1
Chingiz Nazar <chingiz.sultanbekuly@gmail.com>
2023-04-13 20:26:02 +0500

    leetcode - sql badge 1 - 627 - solved

--- Leetcode - Database/1Easy/0627. Swap Salary.sql
+++ Leetcode - Database/1Easy/0627. Swap Salary.sql
@@ -1,7 +1,15 @@
-# Write your MySQL query statement 
+--# Write your MySQL query statement 
 UPDATE Salary
 SET sex = 
   CASE 
     WHEN sex='m' THEN 'f'
     WHEN sex='f' THEN 'm'
   END
+
+----------------
+
+/* Write your T-SQL query statement below */
+UPDATE Salary 
+SET sex = 
+    CASE WHEN sex = 'm' THEN 'f'
+    ELSE 'm' END
