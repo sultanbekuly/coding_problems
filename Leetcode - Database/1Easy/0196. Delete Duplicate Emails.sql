a1aeb19bc96cb19c78169c7aaddbaa74c1d79f39
Chingiz Nazar <chingiz.sultanbekuly@gmail.com>
2023-04-13 20:56:49 +0500

    leetcode - sql badge 1 - 196 - solved

--- Leetcode - Database/1Easy/0196. Delete Duplicate Emails.sql
+++ Leetcode - Database/1Easy/0196. Delete Duplicate Emails.sql
@@ -10,3 +10,18 @@ INNER JOIN
   HAVING COUNT(*) > 1) p2
   ON p1.email = p2.email AND p1.id != p2.min_id
 
+
+-----------------------------------------------
+/* 
+ Please write a DELETE statement and DO NOT write a SELECT statement.
+ Write your T-SQL query statement below
+ */
+ DELETE p1 FROM 
+ Person p1
+ INNER JOIN   
+    (SELECT 
+    email, MIN(id) as min_id
+    FROM Person
+    GROUP BY email
+    HAVING COUNT(*)>1) p2
+    ON p1.email = p2.email AND p1.id != p2.min_id
