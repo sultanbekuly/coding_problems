9f001dcb85033d8b3422b35422f12135de498ab0
Chingiz Nazar <chingiz.nazar@kaztour.kz>
2025-09-04 13:53:05 +0500

    algomap.io > 1_Arrays&Strings > 6 - 14

---
+++ algomap.io/1_Arrays&Strings/6_14. Longest Common Prefix.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/longest-common-prefix/description/
+
+# TODO: break for loop (pass, continue)
+
+class Solution:
+	def longestCommonPrefix(self, strs: list[str]) -> str:
+		if len(strs) == 1:
+			return strs[0]
+		prefix = list(strs[0])
+		for s in strs:
+			len_s = len(s)
+			len_p = len(prefix)
+			len_smallest = len_s if len_s < len_p else len_p
+			prefix = prefix[0:len_smallest] # cut prefix by smallest len
+			for i in range(len_smallest):
+				if s[i] != prefix[i]:
+					prefix = s[0:i] # cut prefix by discrepency
+					break
+			
+		return "".join(prefix)
+		# Time: O(N*M) N - len(strs), M - len(str)
+		# Space: O(1)	
+	
+
+
+# Tests (input, expected output)
+tests = [
+	(["flower","flow"], "flow"),
+	(["flower","flow","flight"], "fl"),
+	(["dog","racecar","car"], ""),
+	(["dog"], "dog"),
+	(["ab", "a"], "a"),	
+]
+for inp1, expected in tests:
+	out = Solution().longestCommonPrefix(inp1)
+	status = "PASSED" if expected==out else "FAILED"
+	print(f"{status}! Input: {(inp1)} -> {out} (expected {expected})")
